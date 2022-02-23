c275d447c7ac94cabcdda43fcd550381941523f1
YaroslavKSE <ikhomych@kse.org.ua>
2022-02-23 14:11:24 +0200

    Added HuffmanTreeBuilder and Node class

---
+++ Task_4/Node.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_4
+{
+    public class Node
+    {
+        public string Value { get; set; }
+        public int Frequency { get; set; }
+        public Node Left = null;
+        public Node Right = null;
+
+        public Node(string value, int frequency, Node left, Node right)
+        {
+            Value = value;
+            Frequency = frequency;
+            Left = left;
+            Right = right;
+
+        }
+    }
+}
--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-
+using System.Linq;
+using Task_4;
 
 var elementsFrequency = new Dictionary<char, int>();
 
@@ -19,6 +20,28 @@ foreach (var line in File.ReadAllLines(@"D:\C#\Task_4\Task_4\dictionary.txt"))
             elementsFrequency.Add(variable, 1);
         }
     }
+}
+
+
+var frequencyQueue = new PriorityQueue<Node, int>();
+
+foreach (var VARIABLE in elementsFrequency)
+{
+    frequencyQueue.Enqueue(new Node(VARIABLE.Key.ToString(), VARIABLE.Value, null, null), VARIABLE.Value);    
+}
+
+var HuffmanTree = new PriorityQueue<Node, int>();
+
+
+while (frequencyQueue.Count > 1)
+{
+    var first = frequencyQueue.Dequeue();
+    var second = frequencyQueue.Dequeue();
+    if (first.Frequency <= second.Frequency)
+        {
+            var newNode = new Node(first.Value + second.Value, first.Frequency + second.Frequency, first, second);
+            frequencyQueue.Enqueue(newNode, first.Frequency + second.Frequency);
+        }
 }
 
 Console.WriteLine();
