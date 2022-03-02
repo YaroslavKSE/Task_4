f7234687c9c79f5a4b94c64109556659c7cb6101
YaroslavKSE <ikhomych@kse.org.ua>
2022-03-02 11:57:20 +0200

    Upgraded "Node" class

---
+++ Task_4/Node.cs
@@ -0,0 +1,19 @@
+﻿namespace Task_4
+{
+    public class Node
+    {
+        public string Value { get; }
+        public int Frequency { get; }
+        public readonly Node Left;
+        public readonly Node Right;
+
+        public Node(string value, int frequency, Node left, Node right)
+        {
+            Value = value;
+            Frequency = frequency;
+            Left = left;
+            Right = right;
+
+        }
+    }
+}
