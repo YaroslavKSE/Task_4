67df7a232637b60717b40d3976844d99c22d31ad
Maxim Pozdniakov <94074148+qualia4@users.noreply.github.com>
2022-04-03 00:40:29 +0300

    Update Program.cs

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -1,72 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
 using Task_4;
+using TextReader = Task_4.TextReader;
 
-// ------ GlobalVariables ------
+var frequencyReader = new FrequencyReader();
+var elementsFrequency = frequencyReader.Read(new Dictionary<char, int>(), "dictionary.txt");
 
-var elementsFrequency = new Dictionary<char, int>();
 var frequencyQueue = new PriorityQueue<Node, int>();
-var elementsCode = new Dictionary<string, string>();
-var codedFile = new List<string>();
-var text = "";
-var decodedFile = "";
-
-// ------ Structure ------
-
-FrequencyReader(elementsFrequency, @"dictionary.txt");
-
-TextReader(@"dictionary.txt");
-
 SortToQueue();
 
-TreeBuilder();
+var textReader = new TextReader();
+var text = textReader.Read("", @"dictionary.txt");
 
-var rootNode = frequencyQueue.Peek();
-Encode(rootNode, "", elementsCode);
+var builder = new TreeBuilder();
+frequencyQueue = builder.Builder(frequencyQueue);
 
-PrintCodedElements(elementsCode);
+var huffmanTable = new Dictionary<string, string>();
+var encoder = new TreeEncoder();
+encoder.Encode(frequencyQueue.Peek(), "", huffmanTable);
+encoder.PrintCodedElements(huffmanTable);
+
+var codedFile = new List<string>();
 
 ConvertToHuffmanCode(text);
 
+var decoder = new Decoder();
+
+var file = decoder.DecodeFile(frequencyQueue.Peek(), codedFile);
+
 PrintHuffmanCodedFile();
+
 
 WriteHuffmanCodedFile();
 
-DecodeFile(rootNode, codedFile);
-
 Console.WriteLine();
-Console.WriteLine(decodedFile);
-
-
-// ------ Implementation ------
-
-void FrequencyReader(Dictionary<char, int> storeElements,  string path)
-{
-    foreach (var line in File.ReadAllLines(path))
-    {
-        var elements = line.ToCharArray();
-        foreach (var variable in elements)
-        {
-            if (storeElements.ContainsKey(variable))
-            {
-                storeElements[variable] += 1;
-            }
-            else
-            {
-                storeElements.Add(variable, 1);
-            }
-        }
-    }
-}
-
-void TextReader(string path)
-{
-    foreach (var line in File.ReadLines(path))
-    {
-        text += line;
-    }    
-}
+Console.WriteLine(file);
 
 void SortToQueue()
 {
@@ -76,68 +42,11 @@ void SortToQueue()
     }
 }
 
-void TreeBuilder()
-{
-    while (frequencyQueue.Count > 1)
-    {
-        var first = frequencyQueue.Dequeue();
-        var second = frequencyQueue.Dequeue();
-        
-        var newNode = new Node(first.Value + second.Value, first.Frequency + second.Frequency, first, second);
-        frequencyQueue.Enqueue(newNode, first.Frequency + second.Frequency);
-        
-    }
-}
-
-void Encode(Node root, string str, Dictionary<string, string> huffmanCode)
-{
-    if (root == null)
-    {
-        return;
-    }
-
-    if (IsLeaf(root))
-    {
-        huffmanCode.Add(root.Value, str);
-    }
-
-    Encode(root.Left, str + "0", huffmanCode);
-    Encode(root.Right, str + "1", huffmanCode);
-}
-
-bool IsLeaf(Node node)
-{
-    if (node.Left == null && node.Right == null)
-    {
-        return true;
-    }
-    return false;
-}
-
-void PrintCodedElements(Dictionary<string, string> elements)
-{
-    foreach (var element in elements)
-    {
-        Console.Write(element.Key);
-        Console.Write(":");
-        Console.WriteLine(element.Value);
-    }
-}
-
 void ConvertToHuffmanCode(string words)
 {
     foreach (var letter in words)
     {
-        codedFile.Add(elementsCode[letter.ToString()]);
-    }
-}
-
-void WriteHuffmanCodedFile()
-{
-    using StreamWriter writer = new StreamWriter("CodedFile.txt");
-    foreach (var element in codedFile)
-    {
-        writer.Write(element);
+        codedFile.Add(huffmanTable[letter.ToString()]);
     }
 }
 
@@ -149,26 +58,11 @@ void PrintHuffmanCodedFile()
     }
 }
 
-void DecodeFile(Node root, List<string>codedText)
+void WriteHuffmanCodedFile()
 {
-    var current = root;
-    var word = string.Join("", codedText);
-
-    foreach (var bit in word)
+    using StreamWriter writer = new StreamWriter("CodedFile.txt");
+    foreach (var element in codedFile)
     {
-        if (bit == '0')
-        {
-            current = current.Left;
-        }
-        else
-        {
-            current = current.Right;
-        }
-
-        if (current.Right == null && current.Left == null)
-        {
-            decodedFile += current.Value;
-            current = root;
-        }
+        writer.Write(element);
     }
 }
